8fb97000aa41c7745212e113e3ad28a80e5c5277
EvgeniGenchev <genajok@gmail.com>
2021-07-27 15:08:27 +0300

    code refractoring

---
+++ sonos.py
@@ -0,0 +1,203 @@
+try:
+    import requests
+    import re
+    import socket
+    import select
+except:
+    import urequests as requests
+    import ure as re
+    import usocket as socket
+    import uselect as select
+
+from time import sleep
+from sonos_definitions import * #all constant(uppercase) variables
+
+"""
+class SonosDiscovery:
+    Finds Sonos speakers within a local network using Simple Service Discovery Protocol
+"""
+class SonosDiscovery:
+    
+    """
+    Contructor:
+        sets the socket configuration for the Simple Service Discovery Protocol
+    """
+    def __init__(self):
+        self._sock = socket.socket(
+                socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self._sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+
+    
+    """
+    Receive the ip of all the found Sonos speakers
+    @return spearkers list of ip speakers' addresses
+    """
+    def get_speaker_ips(self):
+        speakers = []
+
+        self._sock.sendto(PLAYER_SEARCH, (MCAST_GRP, MCAST_PORT))
+
+        while True:
+            rs, _, _ = select.select([self._sock], [], [], 1)
+            if rs:
+                _, addr = self._sock.recvfrom(2048)
+                speakers.append(addr[0])
+            else:
+                break
+        return speakers
+
+"""
+class Sonos:
+    Sonos Speaker controller
+"""
+class Sonos:
+    """
+    Constructor:
+        finds the ip address of the speaker with given name 
+        @param name name of the speaker
+    """
+    def __init__(self, name):
+        ips = SonosDiscovery().get_speaker_ips()
+        self.ip = None
+        for ip in ips:
+            link = "http://" + ip + ":1400/status/zp"
+            xml = requests.get(link).text
+            if self._get_name(xml) == name:
+                self.ip = ip
+                break
+        self.http = "http://" + self.ip + ":1400/"
+
+    
+    """
+    Extracts the ZoneName from  <ip>:1400/status/zp xml respnose
+        @param xml <ip>:1400/status/zp xml respnose
+        @return name the name of the speaker
+    """
+    def _get_name(self, xml):
+        name = xml.split("ZoneName>")[1][:-2]
+        return name
+
+    
+    """
+    Turns the volume up
+        @param value integer to increment the volume
+    """
+    def volume_up(self, value):
+        new_volume = self.volume + value
+        
+        if new_volume > 100:
+            new_volume = 100
+        
+        vc = VOLUME_CHNG_XML_1 + str(new_volume) + VOLUME_CHNG_XML_2
+        
+        self.__run_rdctrl(vc, VOLUME_CHNG_HDRS)
+
+    
+    """
+    Turns the volume down
+        @param value integer to dencrement the volume
+    """
+    def volume_down(self, value):
+        new_volume = self.volume - value
+        
+        if new_volume < 0:
+            new_volume = 0
+        
+        vc = VOLUME_CHNG_XML_1 + str(new_volume) + VOLUME_CHNG_XML_2
+        
+        self.__run_rdctrl(vc, VOLUME_CHNG_HDRS)
+    
+    
+    """
+    Send play command to Sonos
+    """
+    def play(self):
+        self.__run_avt(PLAY_HDRS, PLAY_XML)
+
+    
+    """
+    Send stop command to Sonos
+    """
+    def stop(self):
+        self.__run_avt(STOP_HDRS, STOP_XML)
+
+    
+    """
+    Send pause command to Sonos
+    """
+    def pause(self):
+        self.__run_avt(PAUSE_HDRS, PAUSE_XML)
+
+    
+    """
+    Send next command to Sonos
+    """
+    def next(self):
+        self.__run_avt(NEXT_HDRS, NEXT_XML)
+
+    
+    """
+    Send previous command to Sonos
+    """
+    def previous(self):
+        self.__run_avt(PREV_HDRS, PREV_XML)
+
+    
+    """
+    Send data to <ip>:1400/MediaRenderer/AVTransport/Control
+        @param xml xml configuration
+        @param hdr http headers
+        @return html response body
+    """
+    def __run_avt(self, hdr, xml):
+        link = self.http + "MediaRenderer/AVTransport/Control"
+        return requests.post(link, headers=hdr, data=xml)
+    
+    
+    """
+    Property function for getting the current speaker state
+        @return string of the current speaker state
+    """
+    @property
+    def state(self):
+        r = self.__run_avt(STATE_HDRS, STATE_XML)
+        return r.text.split("CurrentTransportState>")[1][:-2]
+    
+    
+    """
+    Property fuction for getting the current volume
+        @return integer value of the curretn volume
+    """
+    @property
+    def volume(self):
+        r = False
+        r = self.__run_rdctrl(VOLUME_XML, VOLUME_HDRS)
+        return int(re.findall(r'[0-9]+', r.text)[1]) 
+    
+    
+    """
+    Send data to <ip>:1400/MediaRenderer/RenderingControl/Control
+        @param xml xml configuration
+        @param hdr http headers
+        @return html response body
+    """
+    def __run_rdctrl(self, xml, hdr):
+        link = self.http + "MediaRenderer/RenderingControl/Control"
+        return requests.post(link, headers=hdr, data=xml)
+
+"""
+test example
+"""
+if __name__ == "__main__":
+    s = Sonos('Office')
+    print(s.state)
+    s.play()
+    s.stop()
+    s.play()
+    sleep(1)
+    s.volume_up(10)
+    s.next()
+    sleep(4)
+    s.volume_down(10)
+    s.previous()
+    s.pause()
---
+++ sonos_definitions.py
@@ -0,0 +1,82 @@
+"""
+Predifined constants to be imported in sonos.py
+"""
+
+
+XML ="""<?xml version="1.0"?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body>{}</s:Body></s:Envelope>
+"""
+
+PLAYER_SEARCH = b"""M-SEARCH * HTTP/1.1
+HOST: 239.255.255.250:reservedSSDPport
+MAN: ssdp:discover
+MX: 1
+ST: urn:schemas-upnp-org:device:ZonePlayer:1"""
+
+MCAST_GRP = "239.255.255.250"
+
+MCAST_PORT = 1900
+
+NEXT_XML = """<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body><u:Next xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Next></s:Body></s:Envelope>
+"""
+
+PREV_XML = """<?xml version="1.0"?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body><u:Previous xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Previous></s:Body></s:Envelope>
+"""
+
+PLAY_XML = """<?xml version="1.0"?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body><u:Play xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Play>
+</s:Body></s:Envelope>
+"""
+
+STOP_XML = """<?xml version="1.0"?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body><u:Stop xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Stop>
+</s:Body></s:Envelope>
+"""
+
+PAUSE_XML = """<?xml version="1.0"?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body><u:Pause xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Pause>
+</s:Body></s:Envelope>
+"""
+
+STATE_XML = """<?xml version="1.0"?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body><u:GetTransportInfo xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID></u:GetTransportInfo>
+</s:Body></s:Envelope>
+"""
+VOLUME_XML = """<?xml version="1.0"?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body><u:GetVolume xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"><InstanceID>0</InstanceID><Channel>Master</Channel></u:GetVolume>
+</s:Body></s:Envelope>
+"""
+VOLUME_CHNG_XML_1 = """<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:SetVolume xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"><InstanceID>0</InstanceID><Channel>Master</Channel><DesiredVolume>"""
+VOLUME_CHNG_XML_2 = """</DesiredVolume></u:SetVolume></s:Body></s:Envelope>"""
+
+
+VOLUME_HDRS ={'Content-Type': 'text/xml; charset="utf-8"',"soapaction":"urn:schemas-upnp-org:service:RenderingControl:1#GetVolume"}
+PLAY_HDRS = {'Content-Type': 'text/xml; charset="utf-8"',"soapaction":"urn:schemas-upnp-org:service:AVTransport:1#Play"}
+STOP_HDRS = {'Content-Type': 'text/xml; charset="utf-8"',"soapaction":"urn:schemas-upnp-org:service:AVTransport:1#Stop"}
+PAUSE_HDRS = {'Content-Type': 'text/xml; charset="utf-8"',"soapaction":"urn:schemas-upnp-org:service:AVTransport:1#Pause"}
+NEXT_HDRS = {'Content-Type': 'text/xml; charset="utf-8"',"soapaction":"urn:schemas-upnp-org:service:AVTransport:1#Next"}
+PREV_HDRS = {'Content-Type': 'text/xml; charset="utf-8"',"soapaction":"urn:schemas-upnp-org:service:AVTransport:1#Previous"}
+VOLUME_CHNG_HDRS = {'Content-Type': 'text/xml; charset="utf-8"',"soapaction":"urn:schemas-upnp-org:service:RenderingControl:1#SetVolume"}
+STATE_HDRS = {'Content-Type' : 'text/xml; charset="utf-8"', "soapaction": "urn:schemas-upnp-org:service:AVTransport:1#GetTransportInfo"}
+
+
+
+
+
